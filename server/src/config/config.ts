15809a19640507a57d3b245b3cfb862876d0c117
KehindeBello <adedotunomomeji@gmail.com>
2022-11-20 02:46:30 +0100

    add: budget routes

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,5 +1,6 @@
 export const config = {
   PORT: process.env.PORT || 4141,
-  MONGO_URI: 'mongodb://bujit-db:27017/bujit',
+  // MONGO_URI: 'mongodb://bujit-db:27017/bujit',
+  MONGO_URI: 'mongodb://127.0.0.1:27017',
   JWT_SECRET: process.env.JWT_SECRET || 'secret',
 };
--- server/src/routes/budget.router.ts
+++ server/src/routes/budget.router.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import { BudgetController } from '../components/budget/budget.controller';
+import {validateToken} from "../middleware/auth.middleware";
 
+const budgetController = new BudgetController()
 const router: Router = Router();
 
-router.get('/');
+router.post('/create',validateToken, budgetController.createBudget);
+router.get('/fetch',validateToken ,budgetController.getAllBudget);
+router.get('/fetch/:id',validateToken ,budgetController.getBudget);
 
-export const BudgetRouter: Router = router;
+const BudgetRouter: Router = router;
+export default BudgetRouter;
+ 
