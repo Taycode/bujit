ea9cd29ae8f49fc48490d57ae2d09a53de0d222a
Abdulmateen Tairu <tay2druh@gmail.com>
2022-11-20 16:16:37 +0100

    finish bank endpoints

--- server/src/components/bank/bank.controller.ts
+++ server/src/components/bank/bank.controller.ts
@@ -1,7 +1,8 @@
 import {Response} from "express";
 import {ICustomRequest} from "../../interface/custom-request.interface";
 import {CreateBankDto} from "./dto/create-bank.dto";
-import {createBank, fetchUserBanks} from "./bank.service";
+import {createBank, fetchUserBanks, validateBankDetails} from "./bank.service";
+import {ValidateBankDto} from "./dto/validate-bank.dto";
 
 export class BankController {
     async createBank(req: ICustomRequest, res: Response) {
@@ -23,5 +24,22 @@ export class BankController {
             message: 'Banks fetched successfully',
             data: banks,
         });
+    }
+
+    async validateBank(req: ICustomRequest, res: Response) {
+        const payload: ValidateBankDto = req.body;
+        try {
+            const validatedAccount = await validateBankDetails(payload);
+            return res.status(200).json({
+                status: true,
+                message: 'Bank details verified',
+                data: { name: validatedAccount.payload.accountName },
+            });
+        } catch (_e) {
+            return res.status(400).json({
+                status: false,
+                message: 'Bank details could not be verified',
+            });
+        }
     }
 }
